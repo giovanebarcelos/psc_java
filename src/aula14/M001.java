dc97ff89a3cc31cc2f2babd150d13b6ac6ad7d5a
giovane_barcelos <giovane_barcelos@CA-SAL105-50891.uniritter.local>
2023-03-31 20:09:43 -0300

    Extensão Sobremesa

--- src/aula14/M001.java
+++ src/aula14/M001.java
@@ -14,7 +14,7 @@ package aula14;
 public class M001 {
 
 	public static void main(String[] args) {
-		
+	
 
 	}
 
---
+++ src/aula14/Sobremesa.java
@@ -0,0 +1,9 @@
+package aula14;
+
+public class Sobremesa extends Caloria {
+
+	public Sobremesa(String descricao, int qtdeCalorias) {
+		super(descricao, qtdeCalorias);
+	}
+
+}
